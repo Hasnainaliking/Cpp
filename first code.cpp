8825ef4ace374f14ce093085669001fc69f13399
Hasnainaliking <118014123+Hasnainaliking@users.noreply.github.com>
2025-05-18 16:10:49 +0500

    add marks
    --------

--- first code.cpp
+++ first code.cpp
@@ -7,4 +7,16 @@ int main()
 	int valueTwo;
 	int sum = valueOne+valueTwo;
 	int ses;
+	
+   int marks;
+   cout<<"enter marks";
+   cin>>marks;
+ 
+ if (marks>35)
+ {
+ cout<<"you are pass";	
+ }
+ else {
+ 	cout<<"fail";
+ }
 }
